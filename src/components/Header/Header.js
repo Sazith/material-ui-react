62586b652f239f779bbd649e6e70e4b8b0a7de5f
Sazith <sazith.simecsoftdev@gmail.com>
2023-12-12 10:07:10 +0600

    api issue found

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import News from '../News/News';
 const Header = () => {
   const [article,setArticle] = useState([])
   useEffect(() =>{
-    const baseUrl = 'https://newsapi.org/v2/everything?q=tesla&from=2023-11-11&sortBy=publishedAt&apiKey=95ce33b2f7974c5681d4e3e8d111a54c';
+    const baseUrl = 'https://newsapi.org/v2/everything?q=tesla&from=2023-11-12&sortBy=publishedAt&apiKey=95ce33b2f7974c5681d4e3e8d111a54c';
       fetch(baseUrl)
       .then(response => response.json())
       .then(data => setArticle(data.articles))
